8a1944485b7ad38d2ba28d38ee8537c294aa02d2
Attapon Thanawong <attapon_srem@hotmail.com>
2021-07-19 09:35:27 +0700

    update

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-
-git clone https://gitlab.com/indev-moph/fiber-api.git .
 rm -f go.mod
 rm -f go.sum
 rm -rf .git
+rm -f init.sh
+echo "example package: gitlab.com/indev-moph/fiber-api"
+read -p 'Input Go Package: ' gopack
+go mod init ${gopack}
