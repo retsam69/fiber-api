12a816034c7a1fec3f997245657f47600443274e
Attapon Thanawong <attapon_srem@hotmail.com>
2021-07-21 13:19:32 +0700

    update

--- go.mod
+++ go.mod
@@ -3,10 +3,12 @@ module gitlab.com/indev-moph/fiber-api
 go 1.16
 
 require (
-	github.com/attapon-th/go-pkg/logger v0.0.0-20210714051751-0d235286ee0c
-	github.com/attapon-th/go-pkg/rapidoc v0.0.0-20210714051751-0d235286ee0c
+	github.com/attapon-th/go-pkg/logger v0.0.0-20210720081725-4054a1f3685b
+	github.com/attapon-th/go-pkg/rapidoc v0.0.0-20210720081725-4054a1f3685b
 	github.com/gofiber/fiber/v2 v2.15.0
 	github.com/phuslu/log v1.0.74
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.8.1
+	gorm.io/driver/mysql v1.1.1
+	gorm.io/gorm v1.21.12
 )
--- internal/controller/0controller.go
+++ internal/controller/0controller.go
@@ -1,9 +1,34 @@
 package controller
 
+import (
+	"github.com/phuslu/log"
+	"github.com/spf13/viper"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var (
+	DBConn *gorm.DB
+)
+
 func Init() {
+	// DBConn = ConnectDB()
 
 }
 
-func ConnectDB() {
-
+// Example Mysql
+func ConnectDB() *gorm.DB {
+	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
+	dsn := viper.GetString("db_dsn")
+	// log.Debug().Msg(dsn)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		QueryFields: true,
+	})
+	if err != nil {
+		log.Fatal().Err(err).Msg("error config connect database: `MOPHIC`")
+	}
+	if viper.GetBool("dev") {
+		db = db.Debug()
+	}
+	return db
 }
