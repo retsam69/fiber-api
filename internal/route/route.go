ab037cfcae190a73520aa0dad29cd025453f224e
Attapon Thanawong <attapon_srem@hotmail.com>
2021-08-02 13:22:09 +0700

    update

--- internal/route/route.go
+++ internal/route/route.go
@@ -30,7 +30,7 @@ func Init(app fiber.Router) {
 		HeaderText:  "Service API",
 		RenderStyle: rapidoc.RenderStyle_View,
 		SchemaStyle: rapidoc.SchemaStyle_Table,
-		SpecURL:     fmt.Sprintf("%s//docs/docs/openapi-%s.yaml", UrlPrefix, viper.GetString("version")),
+		SpecURL:     fmt.Sprintf("%s/docs/docs/openapi-%s.yaml", UrlPrefix, viper.GetString("version")),
 		LogoURL:     "https://indev.moph.go.th/blog/wp-content/uploads/2021/03/logo.png",
 	}))
 
