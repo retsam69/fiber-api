7999575d56bf819f7ffe26efa79638446aec9392
Attapon Thanawong <attapon_srem@hotmail.com>
2022-05-09 15:00:27 +0700

    Modified: init.sh

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ rm -f go.mod \
 && rm -f go.sum \
 && rm -rf .git \
 && git init \
-&&  echo "Example GO Module: gitlab.com/indev-moph/fiber-api" \ 
+&& echo "Example GO Module: gitlab.com/indev-moph/fiber-api" \
 && read -p 'Input Go Module: ' gopack \
 && go mod init ${gopack} \
 && make mod-up \
