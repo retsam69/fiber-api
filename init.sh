b9a86241e1a1ae7c9bf83f05b1ded6f689b4f7bb
Attapon Thanawong <attapon_srem@hotmail.com>
2022-05-09 14:57:59 +0700

    Deleted: cmd/gmf/command/encrypt.go, cmd/gmf/command/makefile.go, cmd/gmf/gmf.go Modified: .gitignore, README.MD, init.sh

--- init.sh
+++ init.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-rm -f go.mod
-rm -f go.sum
-rm -rf .git
-git init
-echo "Example GO Module: gitlab.com/indev-moph/fiber-api"
-read -p 'Input Go Module: ' gopack
-go mod init ${gopack}
-make mod-up
-go install ./cmd/gmf
-rm -f init.sh
-rm -rf ./cmd/gmf
+rm -f go.mod \
+&& rm -f go.sum \
+&& rm -rf .git \
+&& git init \
+&&  echo "Example GO Module: gitlab.com/indev-moph/fiber-api" \ 
+&& read -p 'Input Go Module: ' gopack \
+&& go mod init ${gopack} \
+&& make mod-up \
+&& go install github.com/attapon-th/gmf/cmd/gmf \
+&& rm -f init.sh
